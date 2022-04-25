b0db0f96b96261a4dd2ec2456caf102535ec60da
kiang <kiange@gmail.com>
2022-04-25 14:23:24 +0800

    cron script to update sunshine data

---
+++ scripts/cron.php
@@ -0,0 +1,14 @@
+<?php
+$rootPath = __DIR__;
+
+$now = date('Y-m-d H:i:s');
+
+exec("cd {$rootPath} && /usr/bin/git pull");
+
+exec("php -q {$rootPath}/sunshine.php");
+
+exec("cd {$rootPath} && /usr/bin/git add -A");
+
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+
+exec("cd {$rootPath} && /usr/bin/git push origin master");
