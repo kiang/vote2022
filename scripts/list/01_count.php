1697bad6514c5888f5674739d971c32c540dd7e5
kiang <kiange@gmail.com>
2022-09-03 18:33:30 +0800

    to find area with only one candidate

---
+++ scripts/list/01_count.php
@@ -0,0 +1,39 @@
+<?php
+$basePath = dirname(dirname(__DIR__));
+
+$pool = [];
+foreach (glob($basePath . '/raw/list/csv/*.csv') as $csvFile) {
+    $p = pathinfo($csvFile);
+    $parts = explode('111年', $p['filename']);
+    $parts = explode('選舉候選人', $parts[1]);
+    $fname = $parts[0];
+    $fh = fopen($csvFile, 'r');
+    while ($line = fgetcsv($fh, 2048)) {
+        if ($line[0] === '選舉區') {
+            continue;
+        }
+        foreach ($line as $k => $v) {
+            $line[$k] = str_replace(["\n", "\r"], '', $v);
+        }
+        if (!isset($pool[$fname])) {
+            $pool[$fname] = [];
+        }
+        if (!isset($pool[$fname][$line[0]])) {
+            $pool[$fname][$line[0]] = [
+                'count' => 0,
+                'data' => [],
+            ];
+        }
+        ++$pool[$fname][$line[0]]['count'];
+        $pool[$fname][$line[0]]['data'][] = $line;
+    }
+}
+
+foreach ($pool as $fname => $lv1) {
+    foreach ($lv1 as $area => $item) {
+        if ($item['count'] === 1) {
+            $candidate = $item['data'][0];
+            echo "{$fname} {$area} - {$candidate[2]} ({$candidate[3]})\n";
+        }
+    }
+}
